4c9e532a1152e9d261a401b831d4ccba6173f6a3
Hyuain <Hyuain97@163.com>
2020-02-17 20:56:18 +0800

    修改版本和 deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果你想要部署到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:Hyuain/hais-ui.git master:gh-pages
+git push -f git@github.com:Hyuain/hais-ui-new.git master:gh-pages
 
 cd -
