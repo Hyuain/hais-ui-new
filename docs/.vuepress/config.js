a275ed540e70444afa82a08581931b91e19bde29
Hyuain <Hyuain97@163.com>
2020-02-17 19:52:10 +0800

    解决 vuepress 没有 icon 的问题

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 module.exports = {
   title: 'Hais UI',
   base:'/hais-ui/',
@@ -18,5 +20,19 @@ module.exports = {
         children: ['/components/button', '/components/input', '/components/grid', '/components/layout', '/components/toast', '/components/tabs', '/components/popover', '/components/collapse']
       },
     ]
+  },
+  chainWebpack: (config) => {
+    config.module
+      .rule('svg-sprite')
+      .test(/\.svg$/)
+      .include.add(path.resolve(__dirname, '../../src/assets/icons')).end()
+      .use('svg-sprite-loader').loader('svg-sprite-loader').options({extract: false}).end()
+      .use('svgo-loader').loader('svgo-loader')
+      .tap(options => ({...options, plugins: [{removeAttrs: {attrs: 'fill'}}]}))
+    config.plugin('svg-sprite')
+      .use(require('svg-sprite-loader/plugin'), [{plainSprite: true}])
+    config.module
+      .rule('svg')
+      .exclude.add(path.resolve(__dirname, '../../src/assets/icons')).end()
   }
 }
--- src/components/icon.vue
+++ src/components/icon.vue
@@ -6,8 +6,9 @@
 
 <script>
   let importAll = (requireContext) => requireContext.keys().forEach(requireContext)
+
   try {
-    importAll(require.context('@/assets/icons', true, /\.svg$/))
+    importAll(require.context('../assets/icons', true, /\.svg$/))
   } catch (error) {
     console.log(error)
   }
